de9ed51744bfa6e604136539797c357ee2e50f8d
rseyo <rseyo@ou.ac.lk>
2024-03-19 19:22:20 +0530

    Added styles

--- clock.js
+++ clock.js
@@ -1,1 +1,20 @@
 // alert('Analog Clock');
+
+secondDegree = 0;
+minuteDegree= 0;
+hourDegree= 0;
+
+d = new Date();
+
+
+setInterval(
+    function () {
+        d = new Date();
+        secondDegree = d.getSeconds() * 6;
+        minuteDegree = d.getMinutes() * 6;
+        hourDegree = d.getHours() * 30 + Math.round(minuteDegree/12);
+        document.getElementById("second-hand-wrapper").style.transform = "rotate(" + secondDegree + "deg)";
+        document.getElementById("minute-hand-wrapper").style.transform = "rotate(" + minuteDegree + "deg)";
+        document.getElementById("hour-hand-wrapper").style.transform = "rotate(" + hourDegree + "deg)";
+    },1000
+);
