09ccff4a70fa346a384c8f2a52bf938cd7ceadc8
rseyo <rseyo@ou.ac.lk>
2024-03-19 23:16:25 +0530

    Clock size increased. Small clock (Folder small-clock) newly added.

--- small-clock/small-clock.js
+++ small-clock/small-clock.js
@@ -1,12 +1,10 @@
 // alert('Analog Clock');
 
 secondDegree = 0;
-minuteDegree = 0;
-hourDegree = 0;
+minuteDegree= 0;
+hourDegree= 0;
 
 d = new Date();
-
-i = 0;
 
 
 setInterval(
@@ -14,9 +12,9 @@ setInterval(
         d = new Date();
         secondDegree = d.getSeconds() * 6;
         minuteDegree = d.getMinutes() * 6;
-        hourDegree = d.getHours() * 30 + Math.round(minuteDegree / 12);
+        hourDegree = d.getHours() * 30 + Math.round(minuteDegree/12);
         document.getElementById("second-hand-wrapper").style.transform = "rotate(" + secondDegree + "deg)";
         document.getElementById("minute-hand-wrapper").style.transform = "rotate(" + minuteDegree + "deg)";
         document.getElementById("hour-hand-wrapper").style.transform = "rotate(" + hourDegree + "deg)";
-    }, 1000
+    },1000
 );
